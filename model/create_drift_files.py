4a33562ad4a9b460d3873cc2d580cad36f43654a
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-08-06 16:53:07 +0530

    Update create_drift_files.py

--- model/create_drift_files.py
+++ model/create_drift_files.py
@@ -1,35 +1,35 @@
-import pandas as pd
+# import pandas as pd
 
 
-data = pd.read_csv(
-    "data/card_transdata.csv"
-)
+# data = pd.read_csv(
+#     "data/card_transdata.csv"
+# )
 
 
-# Create reference dataset (80%)
-reference = data.sample(
-    frac=0.8,
-    random_state=42
-)
+# # Create reference dataset (80%)
+# reference = data.sample(
+#     frac=0.8,
+#     random_state=42
+# )
 
 
-# Create current dataset (remaining 20%)
-current = data.drop(
-    reference.index
-)
+# # Create current dataset (remaining 20%)
+# current = data.drop(
+#     reference.index
+# )
 
 
-reference.to_csv(
-    "data/reference.csv",
-    index=False
-)
+# reference.to_csv(
+#     "data/reference.csv",
+#     index=False
+# )
 
 
-current.to_csv(
-    "data/current.csv",
-    index=False
-)
+# current.to_csv(
+#     "data/current.csv",
+#     index=False
+# )
 
 
-print("reference.csv created")
-print("current.csv created")
+# print("reference.csv created")
+# print("current.csv created")
